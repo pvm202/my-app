261bdaf59b799afa6709d272476b50b11d9639d3
prashant mahajan <prashantmahajan1440@gmail.com>
2024-06-24 17:56:26 +0530

    Revert "second update"
    This reverts commit a756b62da11102454e9f1942bbb2fa1d8639e1fb.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,32 @@
-import { useState } from 'react';
 import './App.css';
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
 
 function App() {
-
-
-  const[mode,setMode]=useState('light');
-
-  const[alert ,setAlert]=useState(null);
-
-  const showAlert=(message,type)=>{
-        setAlert({
-          msg:message,
-          type:type
-        });
-        setTimeout(() => {
-          setAlert(null);
-        }, 3000);
-
-  }
-
-  const toggleMode=()=>{
-
-    if(mode==='light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#0b2946';
-      showAlert("dark mode enabled","success");
-      document.title="TextUtils-DarkMode";
-      }else{
-      setMode('light');
-      document.body.style.backgroundColor = '#fff';
-      showAlert("light mode enabled","success");
-      document.title="TextUtils-LightMode";
-
-
-    }
-  }
-
   return (
-    <>   
-<Navbar  title="TextUtils" aboutText="this is about textutils"  mode={mode} toggleMode={toggleMode}/>
-
-<Alert alert={alert}/>
-
-
-<div className="container my-3">
-<TextForm  heading="enter the text to analyze" mode={mode} toggleMode={toggleMode} showAlert={showAlert}/>
-{/* <About/> */}
-
-</div>
-
-
-
-  
+    <>
+   <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="/">TextUtils</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/">About</a>
+        </li>
+        
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    
     </>
 
   );
